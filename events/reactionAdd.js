3d89bd8e6555071d99d7afdc8a534e0861732ee3
Baipyrus <simon.waltemathe@gmail.com>
2024-02-04 23:41:04 +0100

    ignore own reactions

--- events/reactionAdd.js
+++ events/reactionAdd.js
@@ -1,7 +1,10 @@
 import { Events } from 'discord.js';
+import { config } from 'dotenv';
+config();
 
 export const name = Events.MessageReactionAdd;
 export async function execute(reaction, user) {
+	if (user.id === process.env.CLIENT) return;
 	const uname = user.username;
 	const rname = reaction._emoji.name;
 	console.debug(`[DEBUG] User '${uname}' reacted with emoji '${rname}'.`);
