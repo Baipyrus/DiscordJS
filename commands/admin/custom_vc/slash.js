c8e3fc2f2c53add43edd46b2e02d69118412fc51
Baipyrus <simon.waltemathe@gmail.com>
2024-02-06 21:26:29 +0100

    initialize custom vc slash command

---
+++ commands/admin/custom_vc/slash.js
@@ -0,0 +1,32 @@
+import { SlashCommandBuilder } from 'discord.js';
+
+export const data = new SlashCommandBuilder()
+    .setName('custom_vc')
+    .setDMPermission(false)
+    .setDescription('Manages reactions for self roles.')
+    .addSubcommand((subcommand) =>
+        subcommand
+            .setName('create')
+            .setDescription('Creates new voice channel.')
+            .addStringOption((option) =>
+                option
+                    .setName('name')
+                    .setRequired(true)
+                    .setDescription('The name to use for the voice channel.')
+            )
+    )
+    .addSubcommand((subcommand) =>
+        subcommand
+            .setName('register')
+            .setDescription('Registers an existing voice channel.')
+            .addStringOption((option) =>
+                option
+                    .setName('id')
+                    .setRequired(true)
+                    .setDescription('The ID to reference the voice channel to be used.')
+            )
+    );
+export async function execute(interaction) {
+    const { options } = interaction;
+    // const id = options.getString('id');
+}
