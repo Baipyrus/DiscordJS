cfc714611ef24410377b65af17bd9c349e7fce9d
Baipyrus <simon.waltemathe@gmail.com>
2024-03-03 00:46:44 +0100

    implement guild member add event

--- events/members/guildMemberAdd.js
+++ events/members/guildMemberAdd.js
@@ -1,7 +1,20 @@
 import { Events, GuildMember } from 'discord.js';
+import { Role } from '../../../database.js';
 
 export const name = Events.GuildMemberAdd;
 /** @param {GuildMember} member */
-export function execute(member) {
-	console.log(member);
+export async function execute(member) {
+	// Find roles to be assigned in guild from database
+	const roles = await Role.findAll({
+		where: {
+			guild: member.guild.id,
+			assign: true
+		}
+	});
+
+	// Ignore if no none found
+	if (roles.length === 0) return;
+
+	// Add roles to member
+	member.roles.add(roles.map((role) => role.id));
 }
