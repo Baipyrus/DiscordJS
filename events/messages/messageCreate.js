cbefb18dcdb3d7b511d8bbad88fcf289694b53fe
Baipyrus <simon.waltemathe@gmail.com>
2024-04-05 20:25:06 +0200

    implemented messageCreate event listener to reply to keywords

--- events/messages/messageCreate.js
+++ events/messages/messageCreate.js
@@ -10,4 +10,44 @@ export async function execute(message) {
 
 	// Split message content into words
 	const words = message.content.split(/\s+/);
+
+	// Get guild keywords
+	/** @type {import('../../models/keywords.js').Keyword[]} */
+	const keywords = await Keywords.findAll({
+		where: {
+			guild: message.guildId,
+			name: {
+				[Op.in]: words
+			}
+		}
+	});
+
+	// Ignore if no keywords found
+	if (keywords.length === 0) return;
+
+	// Get guild responses
+	/** @type {import('../../models/responses.js').Response|null} */
+	const response = await Responses.findOne({
+		where: {
+			keyword: {
+				[Op.in]: keywords.map((keyword) => keyword.id)
+			}
+		},
+		order: sequelize.random()
+	});
+
+	// Ignore if no response found
+	if (response.length === null) return;
+
+	// Send response, selecting exactly one at random
+	await message.reply({
+		content: response.response,
+		allowedMentions: {
+			repliedUser: false
+		}
+	});
+
+	console.info(
+		`[INFO] Responded to keyword with '${response.name}' in guild with ID '${message.guild.id}'.`
+	);
 }
