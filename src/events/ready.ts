ffd3fd48dd3d1e6f12fe8da6c141c84de4a7ea95
Baipyrus <simon.waltemathe@gmail.com>
2026-08-07 12:55:55 +0200

    fix: logging feedback on startup of non-development modes

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,13 @@
 import { Events } from 'discord.js';
-import { logger } from '$lib/Logger.js';
+import { consoleTransport, logger } from '$lib/Logger.js';
 import type { ModifiedClient } from '$lib/Client.js';
 
 export const name = Events.ClientReady;
 export const once = true;
 export function execute(client: ModifiedClient) {
+	// Temporarily allow logging to console to give startup feedback
+	const isDevMode = process.env['NODE_ENV'] === 'development';
+	if (!isDevMode) logger.add(consoleTransport);
 	logger.info(`Ready! Logged in as ${client.user!.tag}`, { label: 'STARTUP' });
+	if (!isDevMode) logger.remove(consoleTransport);
 }
