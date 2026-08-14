43df225ba39cd9dd166090c031e7028dba494a68
Baipyrus <simon.waltemathe@gmail.com>
2026-08-14 12:19:21 +0200

    chore: squash backported changes to fix and parameterize replying

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,8 +1,16 @@
 import { MessageFlags, type RepliableInteraction } from 'discord.js';
 
-export async function replyOrFollowUp(interaction: RepliableInteraction, content: string) {
-	if (interaction.replied || interaction.deferred)
-		await interaction.followUp({ content, flags: MessageFlags.Ephemeral });
+export async function replyOrFollowUp(
+	interaction: RepliableInteraction,
+	content: string,
+	ephemeral: boolean = true
+) {
+	const flags = ephemeral ? MessageFlags.Ephemeral : undefined;
 
-	await interaction.reply({ content, flags: MessageFlags.Ephemeral });
+	if (interaction.replied || interaction.deferred) {
+		await interaction.followUp({ content, flags });
+		return;
+	}
+
+	await interaction.reply({ content, flags });
 }
